009367209fc8cfbfb570bc6eba5a51725a6653cb
essej93 <jesse.s@live.com.au>
2023-09-25 17:11:40 +1000

    - Can now change models and materials for both orbit objects - can now change the rotation/orbit speeds for objects TwBar updated to allow to change above stated variables

--- 3D Animation/main.cpp
+++ 3D Animation/main.cpp
@@ -1,7 +1,9 @@
 // include C++ headers
+#define _USE_MATH_DEFINES
 #include <cstdio>
 #include <iostream>
-//using namespace std;	// to avoid having to use std::
+#include <cmath>
+#include <vector>
 
 #include "utilities.h"
 #include "SimpleModel.h"
@@ -26,12 +28,14 @@ std::map<std::string, glm::mat4> gModelMatrix;			// model matrix
 Light gLight;			// light properties
 
 // Materials Globals
-enum class MaterialType { PEARL, JADE };
-std::map<std::string, Material> gMaterials;
+enum class MaterialType { PEARL, JADE, BRASS };
+std::map<std::string, Material> gMaterials; // stores material values
+std::map<std::string, MaterialType> gSelectedMaterials; // stores the material type for obj 1 and 2
 
 // Model globals
 enum class ModelType { SPHERE, CUBE, SUZANNE };
-std::map<std::string, SimpleModel> gModels;
+std::map<std::string, SimpleModel> gModels; // stores models
+std::map<std::string, ModelType> gSelectedModels; // stores selected model for obj 1 and 2
 
 // shaders global
 std::map<std::string, ShaderProgram> gShaders; // holds multiple shaders
@@ -40,8 +44,30 @@ std::map<std::string, ShaderProgram> gShaders; // holds multiple shaders
 
 // controls
 bool gWireframe = false;	// wireframe control
-float gOrbitSpeed = 0.5f;
+float gOrbitSpeed[2] = { 0.5f, 0.5f };
 float gRotationSpeed[2] = { 1.0f, 1.0f };
+
+// generate vertices for a circle based on a radius and number of slices
+void generate_circle(const float radius, const unsigned int slices, const float scale_factor, std::vector<GLfloat>& vertices)
+{
+	float slice_angle = M_PI * 2.0f / slices;	// angle of each slice
+	float angle = 0;			// angle used to generate x and y coordinates
+	float x, y, z = 0.0f;		// (x, y, z) coordinates
+
+	// generate vertex coordinates for a circle
+	for (int i = 0; i <= slices; i++)
+	{
+		x = radius * cos(angle) * scale_factor;
+		y = radius * sin(angle);
+
+		vertices.push_back(x);
+		vertices.push_back(y);
+		vertices.push_back(z);
+
+		// update to next angle
+		angle += slice_angle;
+	}
+}
 
 // function initialise scene and render settings
 static void init(GLFWwindow* window)
@@ -92,6 +118,12 @@ static void init(GLFWwindow* window)
 	gModelMatrix["OrbitObj1"] = glm::mat4(1.0f);
 	gModelMatrix["OrbitObj2"] = glm::mat4(1.0f);
 
+	// initialise material/model types
+	gSelectedMaterials["Obj1"] = MaterialType::JADE;
+	gSelectedMaterials["Obj2"] = MaterialType::PEARL;
+	gSelectedModels["Obj1"] = ModelType::SUZANNE;
+	gSelectedModels["Obj2"] = ModelType::CUBE;
+
 	// load models
 	gModels["Sphere"].loadModel("./models/sphere.obj");
 	gModels["Cube"].loadModel("./models/cube.obj");
@@ -102,26 +134,39 @@ static void init(GLFWwindow* window)
 static void update_scene(GLFWwindow* window)
 {
 
-	static glm::vec3 moveVec(0.0f);
-	static float orbitAngle = 0.0f;
+	static glm::vec3 staticVec(0.0f);
+	static float orbitAngle[2] = { 0.0f, 0.0f };
 	static float rotationAngle[2] = { 0.0f, 0.0f };
 
-	orbitAngle += gOrbitSpeed * gFrameTime;
+	orbitAngle[0] += gOrbitSpeed[0] * gFrameTime;
+	orbitAngle[1] += gOrbitSpeed[1] * gFrameTime;
 	rotationAngle[0] += gRotationSpeed[0] * gFrameTime;
 	rotationAngle[1] += gRotationSpeed[1] * gFrameTime;
 
-	gModelMatrix["Sphere"] = glm::translate(moveVec);
+	gModelMatrix["Sphere"] = glm::translate(staticVec);
 
 	gModelMatrix["OrbitObj1"] = gModelMatrix["Sphere"]
-		* glm::rotate(orbitAngle, glm::vec3(0.0f, 1.0f, 0.0f))
+		* glm::rotate(orbitAngle[0], glm::vec3(0.0f, 1.0f, 0.0f))
 		* glm::translate(glm::vec3(5.0f, 0.0f, 0.0f))
-		* glm::rotate(rotationAngle[0] - orbitAngle, glm::vec3(0.0f, 1.0f, 0.0f))
+		* glm::rotate(rotationAngle[0] - orbitAngle[0], glm::vec3(0.0f, 1.0f, 0.0f))
 		* glm::scale(glm::vec3(0.7f, 0.7f, 0.7f));
+
+	gModelMatrix["OrbitObj2"] = gModelMatrix["OrbitObj1"] * glm::rotate(orbitAngle[1], glm::vec3(0.0f, 1.0f, 0.0f))
+		* glm::translate(glm::vec3(3.0f, 0.0f, 0.0f))
+		* glm::rotate(rotationAngle[1] - orbitAngle[1], glm::vec3(0.0f, 1.0f, 0.0f))
+		* glm::scale(glm::vec3(0.4f, 0.4f, 0.4f));
+
 }
 
 // function to render the scene
 static void render_scene()
 {
+	// variable to store selected model string
+	std::string selectedModel;
+	
+	// variable to store selected material
+	std::string selectedMaterial;
+
 	// clear colour buffer and depth buffer
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -167,10 +212,21 @@ static void render_scene()
 	gShader->setUniform("uLight.Ld", gLight.Ld);
 	gShader->setUniform("uLight.Ls", gLight.Ls);
 
+	// if statement to determine selected material
+	if (gSelectedMaterials["Obj1"] == MaterialType::BRASS) {
+		selectedMaterial = "Brass";
+	}
+	else if (gSelectedMaterials["Obj1"] == MaterialType::JADE) {
+		selectedMaterial = "Jade";
+	}
+	else if (gSelectedMaterials["Obj1"] == MaterialType::PEARL) {
+		selectedMaterial = "Pearl";
+	}
+
 	// set material properties
-	gShader->setUniform("uMaterial.Ka", gMaterials["Brass"].Ka);
-	gShader->setUniform("uMaterial.Kd", gMaterials["Brass"].Kd);
-	gShader->setUniform("uMaterial.Ks", gMaterials["Brass"].Ks);
+	gShader->setUniform("uMaterial.Ka", gMaterials[selectedMaterial].Ka);
+	gShader->setUniform("uMaterial.Kd", gMaterials[selectedMaterial].Kd);
+	gShader->setUniform("uMaterial.Ks", gMaterials[selectedMaterial].Ks);
 	gShader->setUniform("uMaterial.shininess", gMaterials["Brass"].shininess);
 
 	// set viewing position
@@ -185,30 +241,68 @@ static void render_scene()
 	gShader->setUniform("uModelMatrix", gModelMatrix["OrbitObj1"]);
 	gShader->setUniform("uNormalMatrix", normalMatrix);
 
-	gModels["Suzanne"].drawModel();
+	// if statements to check the object to render
+	if (gSelectedModels["Obj1"] == ModelType::SUZANNE) {
+		selectedModel = "Suzanne";
+	}
+	else if (gSelectedModels["Obj1"] == ModelType::CUBE) {
+		selectedModel = "Cube";
+	}
+	else if (gSelectedModels["Obj1"] == ModelType::SPHERE) {
+		selectedModel = "Sphere";
+	}
+
+	gModels[selectedModel].drawModel();
 
 	// Object 2 render
 
-	//	// set light properties
-	//gShader->setUniform("uLight.dir", gLight.dir);
-	//gShader->setUniform("uLight.La", gLight.La);
-	//gShader->setUniform("uLight.Ld", gLight.Ld);
-	//gShader->setUniform("uLight.Ls", gLight.Ls);
+	// set light properties
+	gShader->setUniform("uLight.dir", gLight.dir);
+	gShader->setUniform("uLight.La", gLight.La);
+	gShader->setUniform("uLight.Ld", gLight.Ld);
+	gShader->setUniform("uLight.Ls", gLight.Ls);
 
-	//// set material properties
-	//gShader->setUniform("uMaterial.Ka", gMaterials["Brass"].Ka);
-	//gShader->setUniform("uMaterial.Kd", gMaterials["Brass"].Kd);
-	//gShader->setUniform("uMaterial.Ks", gMaterials["Brass"].Ks);
-	//gShader->setUniform("uMaterial.shininess", gMaterials["Brass"].shininess);
+	// if statement to determine selected material
+	if (gSelectedMaterials["Obj2"] == MaterialType::BRASS) {
+		selectedMaterial = "Brass";
+	}
+	else if (gSelectedMaterials["Obj2"] == MaterialType::JADE) {
+		selectedMaterial = "Jade";
+	}
+	else if (gSelectedMaterials["Obj2"] == MaterialType::PEARL) {
+		selectedMaterial = "Pearl";
+	}
 
-	//// set viewing position
-	//gShader->setUniform("uViewpoint", glm::vec3(0.0f, 2.0f, 4.0f));
+	// set material properties
+	gShader->setUniform("uMaterial.Ka", gMaterials[selectedMaterial].Ka);
+	gShader->setUniform("uMaterial.Kd", gMaterials[selectedMaterial].Kd);
+	gShader->setUniform("uMaterial.Ks", gMaterials[selectedMaterial].Ks);
+	gShader->setUniform("uMaterial.shininess", gMaterials[selectedMaterial].shininess);
 
-	//// calculate matrices
-	//MVP = gProjectionMatrix * gViewMatrix * gModelMatrix["OrbitObj2"];
-	//normalMatrix = glm::mat3(glm::transpose(glm::inverse(gModelMatrix["OrbitObj2"])));
+	// set viewing position
+	gShader->setUniform("uViewpoint", glm::vec3(0.0f, 2.0f, 4.0f));
 
-	//gModels["Cube"].drawModel();
+	// calculate matrices
+	MVP = gProjectionMatrix * gViewMatrix * gModelMatrix["OrbitObj2"];
+	normalMatrix = glm::mat3(glm::transpose(glm::inverse(gModelMatrix["OrbitObj2"])));
+
+	// set uniform variables
+	gShader->setUniform("uModelViewProjectionMatrix", MVP);
+	gShader->setUniform("uModelMatrix", gModelMatrix["OrbitObj2"]);
+	gShader->setUniform("uNormalMatrix", normalMatrix);
+
+	// if statements to check the object to render
+	if (gSelectedModels["Obj2"] == ModelType::SUZANNE) {
+		selectedModel = "Suzanne";
+	}
+	else if (gSelectedModels["Obj2"] == ModelType::CUBE) {
+		selectedModel = "Cube";
+	}
+	else if (gSelectedModels["Obj2"] == ModelType::SPHERE) {
+		selectedModel = "Sphere";
+	}
+
+	gModels[selectedModel].drawModel();
 
 
 
@@ -278,6 +372,22 @@ TwBar* create_UI(const std::string name) {
 
 	TwBar* twBar = TwNewBar(name.c_str());
 
+	// TwEnum to store possible materials
+	TwEnumVal materialValue[] = {
+	{static_cast<int>(MaterialType::PEARL), "Pearl"},
+	{static_cast<int>(MaterialType::JADE), "Jade"},
+	{static_cast<int>(MaterialType::BRASS), "Brass"}
+	};
+	TwType materialOptions = TwDefineEnum("materialType", materialValue, 3);
+
+	// TwEnum to store possible models
+	TwEnumVal modelValue[] = {
+	{static_cast<int>(ModelType::SPHERE), "Sphere"},
+	{static_cast<int>(ModelType::CUBE), "Cube"},
+	{static_cast<int>(ModelType::SUZANNE), "Suzanne"}
+	};
+	TwType modelOptions = TwDefineEnum("modelType", modelValue, 3);
+
 	// give tweak bar the size of graphics window
 	TwWindowSize(gWindowWidth, gWindowHeight);
 	TwDefine(" TW_HELP visible=false ");	// disable help menu
@@ -297,6 +407,15 @@ TwBar* create_UI(const std::string name) {
 	TwAddVarRW(twBar, "La", TW_TYPE_COLOR3F, &gLight.La, " group='Light' ");
 	TwAddVarRW(twBar, "Ld", TW_TYPE_COLOR3F, &gLight.Ld, " group='Light' ");
 	TwAddVarRW(twBar, "Ls", TW_TYPE_COLOR3F, &gLight.Ls, " group='Light' ");
+
+	TwAddVarRW(twBar, "Model 1", modelOptions, &gSelectedModels["Obj1"], " group='Orbit Object 1' ");
+	TwAddVarRW(twBar, "Material 1", materialOptions, &gSelectedMaterials["Obj1"], " group='Orbit Object 1' ");
+	TwAddVarRW(twBar, "Orbit speed 1", TW_TYPE_FLOAT, &gOrbitSpeed[0], " group='Orbit Object 1' precision=2 step='0.01' max=10.0 min=-10.0 ");
+	TwAddVarRW(twBar, "Rotation speed 1", TW_TYPE_FLOAT, &gRotationSpeed[0], " group='Orbit Object 1' precision=2 step='0.01' max=10.0 min=-10.0 ");
+
+	TwAddVarRW(twBar, "Model 2", modelOptions, &gSelectedModels["Obj2"], " group='Orbit Object 2' ");
+	TwAddVarRW(twBar, "Material 2", materialOptions, &gSelectedMaterials["Obj2"], " group='Orbit Object 2' ");
+	TwAddVarRW(twBar, "Orbit speed 2", TW_TYPE_FLOAT, &gOrbitSpeed[1], " group='Orbit Object 2' precision=2 step='0.01' max=10.0 min=-10.0 ");
 
 
 	return twBar;
