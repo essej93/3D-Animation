1c378cb81dca59533ea7d9e550aff965433080be
essej93 <jesse.s@live.com.au>
2023-10-05 16:02:47 +1100

    finished assignment

--- 3D Animation/main.cpp
+++ 3D Animation/main.cpp
@@ -88,12 +88,6 @@ static void init(GLFWwindow* window)
 
 	glEnable(GL_DEPTH_TEST); // enable debth buffer test
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	
-
 	// link shaders
 	gShaders["Simple"].compileAndLink("simpleColor.vert", "simpleColor.frag");
 	gShaders["Animation"].compileAndLink("animation.vert", "animation.frag");
